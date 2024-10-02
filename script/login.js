ff2ba8a2f0dcd0253447260ceb2a7796c664044c
sdineijr <sdineijr0@gmail.com>
2024-10-02 13:52:13 -0300

    alterações login.js

--- script/login.js
+++ script/login.js
@@ -1,59 +1,47 @@
+// Função assíncrona para realizar o login
 async function login() {
+  
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
 
+  
   if (!email || !password) {
     alert("Todos os campos devem ser preenchidos");
   } else {
+    
     const data = {
       email: email,
       password: password,
-      user_type_id: 1, // Mantém o user_type_id como padrão
+      user_type_id: 1, 
     };
 
     try {
+      
       let url = "https://go-wash-api.onrender.com/api/login";
+
       const response = await fetch(url, {
-        method: "POST",
+        method: "POST", 
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/json", 
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data), 
       });
 
-      console.log(response.status);
-
       const result = await response.json();
-
-      console.log(result);
-      console.log(result.status);
-
+      if (response.ok) {
+        
+        localStorage.setItem("user", JSON.stringify(result));
+        window.location.href = "home.html";
+        return
+      }
+  
       if (result && result.data && result.data.errors) {
         const errors = result.data.errors;
+        alert(errors);
+        return
 
-        if (errors === "Usuário não está ativo") {
-          return alert("Usuário não está ativo");
-        } else if (errors === "Usuário não foi encontrado") {
-          return alert("Usuário não foi encontrado");
-        } else {
-          return alert("Erro: " + errors);
-        }
       }
 
-      if (result.access_token) {
-        localStorage.setItem("access_token", result.access_token);
-        localStorage.setItem("user", result.user.id);
-
-        const origin = window.location.origin;
-        console.log(origin);
-
-        const currentLocation = window.location.href;
-        console.log(currentLocation);
-
-        window.location.href = "home.html";
-      } else {
-        alert("Erro: " + result.message);
-      }
     } catch (error) {
       console.log(error);
     }

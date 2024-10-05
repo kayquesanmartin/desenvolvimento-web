3a84b0ded0f51d2b7613f38993c65372819398a3
sdineijr <sdineijr0@gmail.com>
2024-10-05 00:29:54 -0300

    atualização do CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./view/*.html",
+    "./script/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

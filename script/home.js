bb1eeb686add20252761763c17333571ac2bfac7
sdineijr <sdineijr0@gmail.com>
2024-10-10 18:47:29 -0300

    Alterações no CSS e HTML

--- script/home.js
+++ script/home.js
@@ -24,7 +24,30 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
+// Função que é chamada quando o botão de logout é clicado
+document.getElementById("logout_btn").addEventListener("click", function() {
+  
+  // Remover dados de autenticação (exemplo com localStorage)
+  localStorage.removeItem("user");  // Se você estiver usando localStorage para armazenar o token do usuário
+  sessionStorage.removeItem("userSession");  // Se você estiver usando sessionStorage para sessão temporária
+  
+  // Redirecionar o usuário para a página de login ou home
+  window.location.href = "../view/login.html";  // Altere para a página de login ou home desejada
+});
 
+document.addEventListener("DOMContentLoaded", function() {
+  // Recupera o objeto do usuário armazenado no localStorage
+  const user = JSON.parse(localStorage.getItem("user")); // Assumindo que os dados do usuário estão no localStorage como um objeto JSON
+
+  if (user && user.user.email) {
+    // Se o objeto de usuário existir e tiver o campo 'name', exibimos no header
+    document.getElementById("user-email-nav").textContent = user.user.email;
+  } else {
+    // Caso não haja dados no localStorage ou nome, pode exibir uma mensagem padrão
+    document.getElementById("user-email-nav").textContent = "Bem-vindo, visitante!";
+  }
+
+});
 
 
 
